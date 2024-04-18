5c6ea3deaa0ab01c30a436353d912b4d337284cc
Daniel1298D <166690480+Daniel1298D@users.noreply.github.com>
2024-04-18 11:20:27 +0200

    wip: add function remove user

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from models.data import users
-from utils.crud import show_users,add_new_user
-
+from utils.crud import show_users,add_new_user,search_users,remove_users
 
 if __name__ == "__main__":
     print("witaj użytkowniku")
@@ -8,12 +7,18 @@ if __name__ == "__main__":
         print("MENU:")
         print("0. zakończ program")
         print("1. wyswietl co u znajomych")
-        menu_option: str = input("dokonaj wyboru:")
         print("2.Dodaj użytkownika")
+        print("3. znajdź użytkownika")
         if menu_option == "0":
             print("program kończy pracę")
+            print("4. usuń użytkownika")
+            menu_option: str = input("dokonaj wyboru:")
             break
         if menu_option == "1":
             show_users(users)
         if menu_option == "2":
             add_new_user(users)
+        if menu_option == "3":
+            search_user(users)
+        if menu_option == "4":
+            remove_users(users)
