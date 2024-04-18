9c75f331fb6137b0fc07b6c0aa08ec9501b3ca4d
Daniel1298D <166690480+Daniel1298D@users.noreply.github.com>
2024-04-18 08:44:09 +0200

    wip: add user printing funcionality

---
+++ main.py
@@ -0,0 +1,17 @@
+imie="Dawid"
+nazwisko="Bałuka"
+liczba_postow=6000
+
+print(f"twój znajomy {imie} {nazwisko} opublikowal {liczba_postow} postów")
+
+imie_2="Kewin"
+nazwisko_2="Czajkowski"
+liczba_postow_2=6002
+
+print(f"twój znajomy {imie_2} {nazwisko_2} opublikowal {liczba_postow_2} postów")
+
+imie_3="Kamil"
+nazwisko_3="Gil"
+liczba_postow_3=1000000
+
+print(f"twój znajomy {imie_3} {nazwisko_3} opublikowal {liczba_postow_3} postów")
