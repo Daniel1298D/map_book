33f90b04a95c9bfca9f75189292caec20a6bc211
Daniel1298D <166690480+Daniel1298D@users.noreply.github.com>
2024-04-18 10:49:08 +0200

    wip: create a function to create users

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from models.data import users
-from utils.crud import show_users
+from utils.crud import show_users,add_new_user
 
 
 if __name__ == "__main__":
     print("witaj użytkowniku")
     while True:
         print("MENU:")
+        print("0. zakończ program")
         print("1. wyswietl co u znajomych")
         menu_option: str = input("dokonaj wyboru:")
+        print("2.Dodaj użytkownika")
         if menu_option == "0":
             print("program kończy pracę")
             break
         if menu_option == "1":
             show_users(users)
+        if menu_option == "2":
+            add_new_user(users)
